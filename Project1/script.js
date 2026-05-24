33e2fa6902e2d90230c020600ac2cc6e075fe96b
Arati-06-Powar <aratipowar2106@gmail.com>
2026-05-24 12:01:44 +0530

    Add files via upload

---
+++ Project1/script.js
@@ -0,0 +1,26 @@
+let display = document.getElementById("display");
+
+function appendValue(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function deleteLast() {
+  display.value = display.value.slice(0, -1);
+}
+
+function calculateResult() {
+  try {
+    if (display.value === "") {
+      alert("Please enter a value");
+      return;
+    }
+
+    display.value = eval(display.value);
+  } catch {
+    display.value = "Error";
+  }
+}
